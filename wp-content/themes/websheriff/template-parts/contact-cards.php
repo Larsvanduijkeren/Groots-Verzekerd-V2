8a2600f3794fb8ee460cbeb6ed689e1031c11623
Lars van Duijkeren <larsvanduijkeren@gmail.com>
2025-11-26 10:22:19 +0100

    new components + styling

--- wp-content/themes/websheriff/template-parts/contact-cards.php
+++ wp-content/themes/websheriff/template-parts/contact-cards.php
@@ -1,4 +1,8 @@
 <?php
+$title = get_field('title');
+$text = get_field('text');
+$cards = get_field('cards');
+
 $id = get_field('id');
 ?>
 
@@ -9,6 +13,35 @@ $id = get_field('id');
     } ?>"
 >
     <div class="container">
-        <h2>contact-cards</h2>
+        <?php if (empty($title) === false) : ?>
+            <div class="intro center" data-aos="fade-up">
+                <h2><?php echo $title; ?></h2>
+
+                <?php if (empty($text) === false) {
+                    echo $text;
+                } ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if (empty($cards) === false) : ?>
+            <div class="flex-wrapper">
+                <?php foreach ($cards as $card) : ?>
+                    <div class="card" data-aos="fade-up">
+                        <?php if (empty($card['icon']) === false) : ?>
+                            <img class="style-svg" src="<?php echo $card['icon']['sizes']['medium']; ?>"
+                                 alt="<?php echo $card['icon']['alt']; ?>">
+                        <?php endif; ?>
+
+                        <?php if (empty($card['title']) === false) : ?>
+                            <h3><?php echo $card['title']; ?></h3>
+                        <?php endif; ?>
+
+                        <?php if (empty($card['text']) === false) {
+                            echo $card['text'];
+                        } ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
     </div>
 </section>
--- wp-content/themes/websheriff/template-parts/emergency-contacts.php
+++ wp-content/themes/websheriff/template-parts/emergency-contacts.php
@@ -1,4 +1,8 @@
 <?php
+$title = get_field('title');
+$text = get_field('text');
+$cards = get_field('cards');
+
 $id = get_field('id');
 ?>
 
@@ -9,6 +13,32 @@ $id = get_field('id');
     } ?>"
 >
     <div class="container">
-        <h2>emergency-contacts</h2>
+        <?php if (empty($title) === false) : ?>
+            <div class="intro center" data-aos="fade-up">
+                <h2><?php echo $title; ?></h2>
+
+                <?php if (empty($text) === false) {
+                    echo $text;
+                } ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if (empty($cards) === false) : ?>
+            <div class="flex-wrapper">
+                <?php foreach ($cards as $card) : ?>
+                    <div class="card" data-aos="fade-up">
+                        <?php if (empty($card['title']) === false) : ?>
+                            <h3 class="h5"><?php echo $card['title']; ?></h3>
+                        <?php endif; ?>
+
+<?php if(empty($card['phone']) === false) : ?>
+    <a href="tel:<?php echo $card['phone']; ?>">
+        <?php echo $card['phone']; ?>
+    </a>
+<?php endif; ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
     </div>
 </section>
